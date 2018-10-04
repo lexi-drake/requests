0c8a1e4ae6b1c71f4155cc405620214028d1560c
Alexa Drake <alexa11drake@gmail.com>
2018-10-04 18:40:05 -0500

    added stats

--- requests.go
+++ requests.go
@@ -2,9 +2,10 @@ package requests
 
 import(
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"github.com/tcnksm/go-httpstat"
 )
 
 type RequestHandler struct {
@@ -14,47 +15,47 @@ type RequestHandler struct {
 type RequestHeaders map[string]string
 type RequestBody map[string]string
 
-func (requestHandler *RequestHandler) Get(url string, headers RequestHeaders) (int, string, error) {
+func (requestHandler *RequestHandler) Get(url string, headers RequestHeaders, result interface{}) (int, httpstat.Result, error) {
 	request, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return -1, "", err
-	}
 	
-	return requestHandler.Send(request, headers)
+	if err != nil {
+		return -1, httpstat.Result{}, err
+	}
+
+	return requestHandler.Send(request, headers, result)
 }
 
-func (requestHandler *RequestHandler) Post(targetUrl string, headers RequestHeaders, body RequestBody) (int, string, error) {
+func (requestHandler *RequestHandler) Post(targetUrl string, headers RequestHeaders, body RequestBody, result interface{}) (int, httpstat.Result, error) {
 	jsonValue, err := json.Marshal(body)
 	if err != nil {
-		return -1, "", err
+		return -1, httpstat.Result{}, err
 	}
 
 	request, err := http.NewRequest("POST", targetUrl,strings.NewReader(string(jsonValue)))
 	if err != nil {
-		return -1, "", err
+		return -1, httpstat.Result{}, err
 	}
 	
-	return requestHandler.Send(request, headers)
+	return requestHandler.Send(request, headers, result)
 }
 
-func (requestHandler *RequestHandler) Send(request *http.Request, headers RequestHeaders) (int, string, error) {
+func (requestHandler *RequestHandler) Send(request *http.Request, headers RequestHeaders, result interface{}) (int, httpstat.Result, error) {
 	for key, value := range headers {
 		request.Header.Set(key, value)
 	}
-
+	
+	var stats httpstat.Result
+	context := httpstat.WithHTTPStat(request.Context(), &stats)
+	request = request.WithContext(context)
+		
 	client := http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
-		return -1, "", err
+		return -1, stats, err
 	}
 
 	defer response.Body.Close()
 
-	responseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return response.StatusCode, "", err
-	}
-	
-	body := string(responseBody)
-	return response.StatusCode, body, nil
+	json.NewDecoder(response.Body).Decode(result)
+	return response.StatusCode, stats, nil
 }
